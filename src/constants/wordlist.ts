56f7b4a60b10653cceb1418761b349938c633f1f
harshad <harshad@vertispro.com>
2023-11-20 21:22:32 +0800

    updated wordlist

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -14,9 +14,57 @@ export const WORDSHINTSEXPLNATIONS = [
       'Conduta enganosa realizada por algúem com o objetivo de obter vantagem ilegal.',
   },
   {
-    word: 'desvio',
-    hint: 'A punishing pinch to the pocket',
+    word: 'moral',
+    hint: 'Regras e costumes',
     explanation:
-      'O desvio de valor ou bem pode ser considerado como é destinado diverso do correto.',
+      'A moral tem um carga subjetiva por ser formada por experiências dos indivíduos e pelos pensamentos e hábitos de uma sociedade, que podem variar conforme o momento e o local onde se vive. Em algumas situações é nossa carga moral que permite seguirmos o caminho correto.',
+  },
+  {
+    word: 'etica',
+    hint: 'Agir de forma correta',
+    explanation:
+      'A ética deve estar presente em todos os nossos relacionamentos, pessoais, familiares, profissionais, entre outros. Agir de forma ética significa mais do seguir as regras e normas, mas também com transparência e respeito.',
+  },
+  {
+    word: 'justo',
+    hint: 'Qualidade de quem age correta e imparcialmente',
+    explanation:
+      'Ser justo é uma qualidade de quem age de forma correta e imparcial, considerando muitas vezes todas as circunstâncias antes de chegar a uma conclusão e tomar uma ação, buscando analisar não somente o ato em si mas as motivações e condições.',
+  },
+  {
+    word: 'regra',
+    hint: 'Norma interna',
+    explanation:
+      'As regras são definições que direcionam, dirigem e orientam a conduta de uma grupo de pessoas.',
+  },
+  {
+    word: 'troco',
+    hint: 'Valor recebido quando entregamos um valor maior que o devido',
+    explanation:
+      'A microcorrupção é considerado um elemento que leva à grande corrupção, são pequenos atos ilícitos que, quando normalizados, desestabilizam o conceito de moralidade. Quando recebemos um valor maior do que o devido, estamos praticando uma microcorrupção. Lembre-se, alguém terá que pagar por este valor.',
+  },
+  {
+    word: 'norma',
+    hint: 'Regras escritas',
+    explanation:
+      'A norma é uma regra escrita que, conforme o emissor, pode ter natureza privada (ex. empresa) ou pública (ex. prefeitura), incluindo as leis, decretos, regulamentos internos, entre outros. As normas privadas se aplicam a um determinado grupo de pessoa que decidiram se associar, enquanto as normas públicas são aquelas criadas pelos governos federais, estaduais e municipais.',
+  },
+  {
+    word: 'roubo',
+    hint: 'Pegar sem autorização o que é de outro',
+    explanation:
+      'O roubo e o furto são crimes que ocorrem quando temos a subtração de bens ou valores Popularmente, estes crimes são tratados sem diferença, porém o roubo tem como característica o uso de violência ou grave ameaça.',
+  },
+  {
+    word: 'crime',
+    hint: 'Atividade investigada pela Polícia',
+    explanation:
+      'Os crimes, também chamados de delitos, são situações descritas no Código Penal, onde também constam a penalidade aplicada quando ocorridos. Com isto pretendeu-se proteger a vida, saúde, patrimônio, ordem pública, enfim todos os bens considerados, pela sua importância, merecedores de uma especial proteção pelo Estado.',
+  },
+  {
+    word: 'droga',
+    hint: 'Uma substância que afeta o corpo',
+    explanation:
+      "A primeira etapa da lavagem de dinheiro é conhecida como 'colocação' (placement), na qual dinheiro 'sujo' é inserido nos sistemas legais e financeiros. Por exemplo, traficantes de drogas ocultam seus lucros ao inseri-los no vasto mercado financeiro global e utilizam o dinheiro lavado para financiar suas atividades ilícitas.",
   },
 ]
